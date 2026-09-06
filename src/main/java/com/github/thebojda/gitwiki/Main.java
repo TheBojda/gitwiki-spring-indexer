1e1ca7c22d72f7572cfc91b41d261da552168598
Laszlo Fazekas <thebojda@gmail.com>
2026-09-06 19:32:03 +0200

    Initial Maven project structure

---
+++ src/main/java/com/github/thebojda/gitwiki/Main.java
@@ -0,0 +1,8 @@
+package com.github.thebojda.gitwiki.spring.indexer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("GitWiki Spring Indexer");
+    }
+}
